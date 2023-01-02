02c711b192155154d11c6e10a126d0194760d6b4
wesqu <wesquel@hotmail.com>
2023-01-02 16:59:10 -0300

    initializer

---
+++ src/main/java/br/com/wesquel/restwithspringbootandjavaerudion/RestWithSpringBootAndJavaErudionApplication.java
@@ -0,0 +1,13 @@
+package br.com.wesquel.restwithspringbootandjavaerudion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaErudionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaErudionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wesquel/restwithspringbootandjavaerudion/RestWithSpringBootAndJavaErudionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wesquel.restwithspringbootandjavaerudion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
