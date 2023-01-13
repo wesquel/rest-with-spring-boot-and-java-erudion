5688a3e9a862396aa7c7dd6bdd6a1590114a993d
wesqu <wesquel@hotmail.com>
2023-01-12 22:22:29 -0300

    Swagger added

---
+++ src/main/java/br/com/wesquel/restwithspringbootandjavaerudion/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package br.com.wesquel.restwithspringbootandjavaerudion.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+
+@Configuration
+public class OpenApiConfig {
+    
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+        .info(
+            new Info()
+            .title("RESTful API with java 18 and Srping boot 3")
+            .version("v1")
+            .description("Some description about your API")
+            .termsOfService("github.com/wesquel")
+            .license(
+                new License()
+                .name("Apache 2.0")
+                .url("github.com/wesquel")
+            )
+        );
+    }
+}
