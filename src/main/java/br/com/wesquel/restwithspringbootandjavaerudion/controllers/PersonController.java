340cb4c1142e7b0f8645b7302cfcd98a710df744
wesqu <wesquel@hotmail.com>
2023-01-14 14:00:45 -0300

    Local cors and Swagger test container

--- src/main/java/br/com/wesquel/restwithspringbootandjavaerudion/controllers/PersonController.java
+++ src/main/java/br/com/wesquel/restwithspringbootandjavaerudion/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -87,6 +88,7 @@ public class PersonController {
         return personServices.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(
         value = "/{id}", 
         produces = {
@@ -147,6 +149,7 @@ public class PersonController {
         return personServices.findById(id);
     }
     
+    @CrossOrigin(origins = {"http://localhost:8080", "https://erudio.com.br"})
     @PostMapping(
         produces = {
             MediaType.APPLICATION_JSON, 
